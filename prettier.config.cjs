acb1c06ed65ce3e42640185f3751157d7af0ebda
Vishnuvardhan Reddy <vishnuvardhanganji@gmail.com>
2024-07-26 10:00:21 +0000

    Supabase init

---
+++ prettier.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  semi: false,
+  useTabs: false,
+  singleQuote: false,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  trailingComma: "none",
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^.+\\.scss$",
+    "^.+\\.css$",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "^@/registry/(.*)$",
+    "^@/styles/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^[./]"
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true
+}
